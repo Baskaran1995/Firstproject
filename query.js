e36e144674e2cce77c31f3144079bebb26c707bf
Baskaran1995 <baskarkrish6@gmail.com>
2022-10-08 05:15:42 +0530

    first commit

---
+++ query.js
@@ -0,0 +1,31 @@
+var header = document.querySelector('#main-header');
+
+header.style.borderBotttom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+
+input.value= 'Hello World'
+
+var submit = document.querySelector('input[type="submit']');
+
+submit.value="SEND";
+
+var secondItem = document.querySelector
+
+('.list-group-item:nth-child(2)');
+
+seconditem.style.color = 'coral';
+
+var titles = document.querySelectorAll('.title');
+
+console.log(titles);
+
+titles[0].textcontent ='Hello';
+
+var even= document.querySelectorAll('li:nth-child(even));
+
+for(var i=0;i<even.length; i++){
+
+odd[i].style.backgroundColor= '#green';
+
+}
